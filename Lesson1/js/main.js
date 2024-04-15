b95fbb62d2dd02c9a37436f5558b996bd8b18964
Maxim <max.shavrukov@gmail.com>
2024-04-15 13:34:23 +0300

    second commit

--- Lesson1/js/main.js
+++ Lesson1/js/main.js
@@ -1,15 +1,2 @@
-var stroka = 'word'
-
-switch (stroka) {
-	case 4:
-		console.log('переменная со значением 4')
-		break
-	case '45':
-		console.log('переменная со значением 45')
-		break
-	case 'word':
-		console.log('переменная со значением "word"')
-		break
-	default:
-		console.log('default')
-}
+var arr = [5, true, "stroka", 5.7, 0, -100];
+console.log(arr);
