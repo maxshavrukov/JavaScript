f1844da620fd9e863c8b719125cf03a7643ec13b
Maxim Shavrukov <max.shavrukov@gmail.com>
2024-04-15 11:05:39 +0300

    First commit

---
+++ Lesson1/js/main.js
@@ -0,0 +1,15 @@
+var stroka = 'word'
+
+switch (stroka) {
+	case 4:
+		console.log('переменная со значением 4')
+		break
+	case '45':
+		console.log('переменная со значением 45')
+		break
+	case 'word':
+		console.log('переменная со значением "word"')
+		break
+	default:
+		console.log('default')
+}
